c56c3d81a2cc528dae1df67db19bc645adb5eada
gurumeditation0x0 <gurumeditation0x0@gmail.com>
2026-03-10 12:00:05 +0100

    modif early-init.el

--- emacs.d/.emacs.d/early-init.el
+++ emacs.d/.emacs.d/early-init.el
@@ -1,12 +1,27 @@
-
 ;;; early-init.el --- Early initialization for Emacs -*- lexical-binding: t; -*-
 ;;; License: GPLv3
-;;; This file is loaded before init.el to optimize startup performance
+;;; Chargé avant init.el pour optimiser le démarrage
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 1. Accélération du démarrage (GC)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; early-init.el -*- lexical-binding: t; -*-
+;; Très haut GC pendant le startup pour éviter les pauses
+(setq gc-cons-threshold (* 50 1000 1000)) ;; 50 MB
+(setq gc-cons-percentage 0.6)
 
-;; Détection OS
+;; Restaurer GC normal après init pour stabilité
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 16 1000 1000)) ;; 16 MB
+            (setq gc-cons-percentage 0.1)
+            (garbage-collect)
+            (message "GC restauré à 16MB après démarrage")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 2. Détection de l'OS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setq my-os
       (cond
        ((eq system-type 'windows-nt) 'windows)
@@ -14,7 +29,10 @@
        ((eq system-type 'darwin)     'mac)
        (t 'unknown)))
 
-;; Définition du user-emacs-directory.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 3. Définition des répertoires
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setq user-emacs-directory
       (expand-file-name
        (cond
@@ -24,84 +42,105 @@
          (concat (getenv "HOME") "/.emacs.d")))))
 
 
-(setq home-dir "~") ;; home 
+(setq home-dir 
+      (cond
+       ((eq system-type 'windows-nt) (getenv "USERPROFILE"))
+       ((eq system-type 'gnu/linux)  (getenv "~"))))
+       
+(message home-dir)
+
+
 (setq org-directory (concat home-dir "/org/"))
-;;(setq custom-file (concat user-emacs-directory "/emacs-custom.el"))
-(setq my-elisp-dir (concat user-emacs-directory "/elisp")) ;; pour tes fichiers elisp
-(setq my-config-dir (concat user-emacs-directory "/config")) ;; pour tes fichiers conf
-(setq my-dev-dir (concat home-dir "/dev")) ;; pour tes fichiers dev
+(setq my-elisp-dir (concat user-emacs-directory "/elisp"))
+(setq my-config-dir (concat user-emacs-directory "/config"))
+(setq my-dev-dir (concat home-dir "/dev"))
 
-(message "home dir %s" home-dir)
-(message "emacs directory %s" user-emacs-directory)
-
-;; Ajouter dossier elisp au load-path
 (add-to-list 'load-path my-elisp-dir)
 (add-to-list 'load-path my-config-dir)
 (add-to-list 'load-path my-dev-dir)
 
+;; (when (eq system-type 'windows-nt)
+;;   (add-to-list 'exec-path "C:/Program Files/Git/usr/bin"))
+
+
+;; Ajout des chemin vers git et ses utilitaires (diff.exe ...)
+;; (when (eq system-type 'windows-nt)
+;; 	  (setenv "PATH" 
+;; 		  (concat
+;; 		   "C:/Program Files/Git/usr/bin/" path-separator
+;; 		   (getenv "PATH")))
+;; 	  (add-to-list 'exec-path "C:/Program Files/Git/usr/bin/"))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Accélération du démarrage
+;; 4. PowerShell / shell UTF-8 (Windows)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Augmenter temporairement le seuil du garbage collector
-;; pour éviter les pauses pendant le chargement des packages
-(setq gc-cons-threshold (* 50 1000 1000)) ;; 50 MB
-(setq gc-cons-percentage 0.6)
+;; (when (eq my-os 'windows-nt)
+;;   ;; Choisir PowerShell si présent
+;;   (let ((xlist
+;;          '("~/AppData/Local/Microsoft/WindowsApps/pwsh.exe"
+;;            "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"))
+;;         xfound)
+;;     (setq xfound (seq-some (lambda (x) (when (file-exists-p x) x)) xlist))
+;;     (when xfound
+;;       (setq explicit-shell-file-name xfound))))
+
+;; Forcer UTF-8 pour tous les processus
+(setq locale-coding-system 'utf-8)
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; UI elements
+;; 5. Interface graphique minimale
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Désactiver dès le début les éléments graphiques
-(tool-bar-mode -1)       ;; Barre d'outils
-(scroll-bar-mode -1)     ;; Barres de défilement
-(menu-bar-mode -1)       ;; Barre de menu
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
 (setq inhibit-startup-screen t)
 
-;; Définir les paramètres par défaut des frames
-;; (setq default-frame-alist
-;;       '((alpha . (85 . 50))   ;; Transparence (85% / 50%)
-;;         ;; (font . "Ubuntu-12") ;; Police par défaut si souhaitée
-;;         ))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Langue et encodage
+;; 6. Langue et encodage (UTF-8 complet)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (set-language-environment "French")
-;; (prefer-coding-system 'utf-8)
-;; (setq default-buffer-file-coding-system 'utf-8)
-;; (setq coding-system-for-write 'utf-8)
-;; (setq buffer-file-coding-system 'utf-8)
-;; (setq-default buffer-file-coding-system 'utf-8-unix)
-;; (setq save-buffer-coding-system 'utf-8-unix)
-;; (setq require-final-newline t)
-
 
 (setenv "LANG" "fr_FR.UTF-8")
 (setq system-time-locale "fr_FR.UTF-8")
-
-;; Langue d'Emacs
 (set-language-environment "French")
-
-;; Encodage UTF-8 global
-(prefer-coding-system 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)
-(setq coding-system-for-write 'utf-8)
-(setq buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8-unix)
 (setq-default buffer-file-coding-system 'utf-8-unix)
-(setq save-buffer-coding-system 'utf-8-unix)
-
 (setq require-final-newline t)
 
-(setq custom-file "~/.emacs.d/emacs-custom.el")
-(setq frame-title-format (list "@" system-name " : %b (%f)"))
-(setq inhibit-startup-screen t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 7. Police adaptée pour accents et emoji
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(cond
+ ((eq my-os 'windows)   ;; Windows
+  (set-face-attribute 'default nil
+                      :family "Consolas"
+                      :height 100)
+  ;; Emoji si besoin
+  ;; (set-fontset-font t 'Emojis"Segoe UI Emoji" nil 'prepend))
+  )
+ ((eq my-os 'gnu/linux)
+  (set-face-attribute 'default nil
+                      :family "Ubuntu Mono"
+                      :height 120)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 8. Paramètres généraux
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq custom-file (expand-file-name "emacs-custom.el" user-emacs-directory))
+(setq frame-title-format '("@" system-name " : %b (%f)"))
 
 (delete-selection-mode 1)
-(global-auto-revert-mode 1)
+(global-auto-revert-mode nil)
+(global-visual-line-mode 1)
+
 (setq debug-on-error nil
       mouse-autoselect-window t
       vc-follow-symlinks t
@@ -111,27 +150,33 @@
       compilation-scroll-output t
       mouse-yank-at-point t)
 
-(global-visual-line-mode t)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Définir les coordonnées de Strasbourg pour le calendrier / sunrise-sunset
+;; 9. Calendrier et lever/coucher soleil (Strasbourg)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq calendar-latitude 48.584667    ;; latitude
-      calendar-longitude 7.736424    ;; longitude
+(setq calendar-latitude 48.584667
+      calendar-longitude 7.736424
       calendar-location-name "Strasbourg, FR")
 
+;; Heure 24h dans la mode line
+(setq display-time-24hr-format t)
+(setq display-time-format "%H:%M")
+(display-time-mode 1)
+
+;; Heure 24h pour calendrier / solar
+(setq calendar-time-display-form
+      '(24-hours ":" minutes
+                  (if time-zone " (" time-zone ")")))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Package management minimal (Emacs 30+)
+;; 10. Gestion minimale des packages (Emacs 30+)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'package)
 (setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-        ("gnu"   . "https://elpa.gnu.org/packages/")
-	("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+      '(("melpa"  . "https://melpa.org/packages/")
+        ("gnu"    . "https://elpa.gnu.org/packages/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 (package-initialize)
-
-;; use-package natif depuis Emacs 30+
 (require 'use-package)
 (setq use-package-always-ensure t)
