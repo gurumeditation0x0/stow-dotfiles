b68f86c7ba0db732f1be5c75aa0d63037a6e3bbf
gurumeditation0x0 <gurumeditation0x0@gmail.com>
2026-05-20 21:40:28 +0200

    Ajout de config-org.el config-org-babel.el

--- emacs.d/.emacs.d/config/config-org.el
+++ emacs.d/.emacs.d/config/config-org.el
@@ -1,28 +1,46 @@
 ;;; config-org.el --- Org general config -*- lexical-binding: t; -*-
 
-;; Affichage de base
+;; Affichage.
 (setq org-src-fontify-natively t)
 (setq org-startup-with-inline-images t)
+(setq org-image-actual-width nil)
 (setq org-show-notification-handler 'ignore)
+(setq org-image-max-width nil)
+(setq org-return-follows-link t)
+(setq org-link-descriptive t)
 
-;; Répertoire org
+;; Répertoire org.
+(setq org-image-actual-width nil)
 (setq org-directory "~/org/")
 
-;; TODO keywords
+;; TODO
+;; Liste des fichiers avec les tâches à faire.
+(setq org-agenda-files (list "~/org/taches1.org"
+                             "~/org/taches2.org"
+                             ))
+
 (setq org-todo-keywords
       '((sequence "A FAIRE" "EN COURS" "|" "FAIT" "ABANDONNÉ")))
 
 (setq org-todo-keyword-faces
       '(("A FAIRE" . org-warning)
         ("EN COURS" . "yellow")
+	("FAIT" . "green")
         ("ABANDONNÉ" . "gray")))
 
-;; Style minimal (tu peux garder ou simplifier)
+;; Style.
 (setq org-hide-emphasis-markers nil
       org-descriptive-links t
       org-hide-leading-stars nil)
 
-;; Images auto-refresh après exécution Babel
+;; ;; Permet de n'afficher que les tâches importantes.
+;;  '(org-agenda-custom-commands
+;;    (quote
+;;     (("1" "Tâches prioritaires"
+;;       ((search "O [#A" nil))
+;;       nil))))
+
+;; Images auto-refresh après exécution Babel.
 (add-hook 'org-babel-after-execute-hook #'org-display-inline-images)
 
 (provide 'config-org)
